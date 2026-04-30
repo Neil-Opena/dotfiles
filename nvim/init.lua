ed22c1211b8afbf7495134974648f959fd7c221c
Neil Opena <neil.opena97@gmail.com>
2026-04-29 22:14:35 -0400

    feat: add telescope plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,12 +30,6 @@ local opts = {}
 
 require("lazy").setup("plugins")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle reveal left<CR>')
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = 'Quit' })
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,12 +1,4 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
-  },
   {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
   {
     "nvim-neo-tree/neo-tree.nvim",
