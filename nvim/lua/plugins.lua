f2bcbd71539072405fc18ba3a65daf4f9a9786e9
Neil Opena <neil.opena97@gmail.com>
2026-04-29 22:21:06 -0400

    chore: move plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,2 @@
 return {
-  {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  }
 }
