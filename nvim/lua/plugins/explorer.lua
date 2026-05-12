ca73b8058a899fe22fc754a47717b5ece4949ab5
Neil Opena <neil.opena97@gmail.com>
2026-05-11 20:37:25 -0400

    chore: update explorer lua

--- nvim/lua/plugins/explorer.lua
+++ nvim/lua/plugins/explorer.lua
@@ -1,26 +1,11 @@
 return {
-  -- Snacks.nvim file explorer - show hidden files
+  -- Snacks.nvim picker/explorer - show hidden files
   {
     "folke/snacks.nvim",
     opts = {
       picker = {
-        hidden = true, -- show hidden/dotfiles
-        ignored = true, -- show gitignored files
-      },
-    },
-  },
-
-  -- Telescope - show hidden files in find_files
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "^.git/" }, -- still ignore .git directory
-      },
-      pickers = {
-        find_files = {
-          hidden = true, -- show dotfiles
-        },
+        hidden = true, -- show hidden/dotfiles in explorer + file search
+        no_ignore = true, -- ignore .gitignore rules, show gitignored files
       },
     },
   },
