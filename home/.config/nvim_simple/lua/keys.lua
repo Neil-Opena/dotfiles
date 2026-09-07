255fc935a6d12a61436091223b6a0f0e2980f205
Neil Opena <neil.opena97@gmail.com>
2026-09-07 00:07:12 -0400

    feat: update explorer

--- home/.config/nvim_simple/lua/keys.lua
+++ home/.config/nvim_simple/lua/keys.lua
@@ -5,3 +5,9 @@ vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Select All' })
 -- pasting over a selection no longer clobbers your clipboard
 vim.cmd([[ xnoremap <expr> p 'pgv"'.v:register.'y' ]])
 
+-- window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Window Left' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Window Down' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Window Up' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Window Right' })
+
--- home/.config/nvim_simple/lua/plugins/navigation.lua
+++ home/.config/nvim_simple/lua/plugins/navigation.lua
@@ -1,10 +1,5 @@
 return {
   {
-    'stevearc/oil.nvim',
-    opts = { view_options = { show_hidden = true } },
-    keys = { { '<leader>e', '<cmd>Oil<cr>', desc = 'File Browser' } },
-  },
-  {
     'folke/snacks.nvim',
     priority = 1000,
     lazy = false,
@@ -24,6 +19,7 @@ return {
       { '<leader>f', function() Snacks.picker.files() end, desc = 'Find Files' },
       { '<leader>s', function() Snacks.picker.grep() end,  desc = 'Search Text' },
       { '<leader>b', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+      { '<leader>e', function() Snacks.explorer() end, desc = 'File Explorer' },
       { 'gd', function() Snacks.picker.lsp_definitions() end, desc = 'Goto Definition' },
     },
   },
