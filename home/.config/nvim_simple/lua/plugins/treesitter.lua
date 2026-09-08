6aa1def10304861c3cff7fa403f6f1fbd9d23704
Neil Opena <neil.opena97@gmail.com>
2026-09-08 12:09:49 -0400

    feat: update treesitter

--- home/.config/nvim_simple/lua/plugins/treesitter.lua
+++ home/.config/nvim_simple/lua/plugins/treesitter.lua
@@ -1,15 +1,19 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
     lazy = false,
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'lua', 'vim', 'javascript', 'typescript', 'python', 'markdown', 'tsx' },
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-    },
+    config = function()
+      local ensure_installed = { 'lua', 'vim', 'javascript', 'typescript', 'python', 'markdown', 'tsx' }
+      require('nvim-treesitter').install(ensure_installed)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+        end,
+      })
+    end,
   },
   {
     'davidmh/mdx.nvim',
